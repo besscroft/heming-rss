4eed770995f9950f7ca418ab226036b87ed7c508
besscroft <besscroft@foxmail.com>
2023-06-20 13:08:29 +0800

    style:菜单栏样式调整

--- components/ui/NavBar.tsx
+++ components/ui/NavBar.tsx
@@ -1,85 +1,16 @@
 import * as React from 'react'
-import List from '@mui/material/List'
-import ListItem from '@mui/material/ListItem'
-import ListItemAvatar from '@mui/material/ListItemAvatar'
-import ListItemText from '@mui/material/ListItemText'
-import FolderIcon from '@mui/icons-material/Folder'
-import { Divider } from '@mui/material'
-import PsychologyIcon from '@mui/icons-material/Psychology'
-import RssFeedIcon from '@mui/icons-material/RssFeed'
-import AddBoxIcon from '@mui/icons-material/AddBox'
-import AutoStoriesIcon from '@mui/icons-material/AutoStories'
-import SmartToyIcon from '@mui/icons-material/SmartToy'
+import { NavList } from '@/components/ui/NavList'
 
 export function NavBar(props: { className?: string }) {
-    const [selectedIndex, setSelectedIndex] = React.useState(1);
-
-    const handleListItemClick = (
-        event: React.MouseEvent<HTMLDivElement, MouseEvent>,
-        index: number,
-    ) => {
-        setSelectedIndex(index);
-    };
     return (
-        <div className={`${props.className} && 'flex flex-col`}>
+        <div className={`${props.className} && 'flex flex-col items-center h-full`}>
             <div className={'h-12'}>
                 <p className="font-serif text-2xl pl-2 pt-2">鹤鸣 RSS</p>
             </div>
-            <div className={'h-[calc(100vh-128px)] flex flex-col space-y-4 pt-2'}>
-                <List subheader={'个人订阅'}>
-                    <ListItem secondaryAction={'23'}>
-                        <ListItemAvatar><RssFeedIcon /></ListItemAvatar>
-                        <ListItemText primary="旅行者的随想" />
-                    </ListItem>
-                    <ListItem secondaryAction={'1'}>
-                        <ListItemAvatar><RssFeedIcon /></ListItemAvatar>
-                        <ListItemText primary="阮一峰周刊" />
-                    </ListItem>
-                    <ListItem secondaryAction={'6'}>
-                        <ListItemAvatar><RssFeedIcon /></ListItemAvatar>
-                        <ListItemText primary="螺莉莉数据中心" />
-                    </ListItem>
-                </List>
-                <Divider />
-                <List subheader={'第三方订阅集'}>
-                    <ListItem secondaryAction={'23'}>
-                        <ListItemAvatar><RssFeedIcon /></ListItemAvatar>
-                        <ListItemText primary="鹤鸣官方订阅集" />
-                    </ListItem>
-                    <ListItem secondaryAction={'23'}>
-                        <ListItemAvatar><RssFeedIcon /></ListItemAvatar>
-                        <ListItemText primary="RSSHUB 订阅集" />
-                    </ListItem>
-                    <ListItem secondaryAction={'1'}>
-                        <ListItemAvatar><RssFeedIcon /></ListItemAvatar>
-                        <ListItemText primary="Nature 期刊订阅" />
-                    </ListItem>
-                    <ListItem secondaryAction={'6'}>
-                        <ListItemAvatar><RssFeedIcon /></ListItemAvatar>
-                        <ListItemText primary="Google 学术" />
-                    </ListItem>
-                </List>
-                <Divider />
-                <List subheader={'工具栏'}>
-                    <ListItem secondaryAction={'23'}>
-                        <ListItemAvatar><AddBoxIcon /></ListItemAvatar>
-                        <ListItemText primary="添加订阅" />
-                    </ListItem>
-                    <ListItem secondaryAction={'23'}>
-                        <ListItemAvatar><AutoStoriesIcon /></ListItemAvatar>
-                        <ListItemText primary="最近阅读" />
-                    </ListItem>
-                    <ListItem secondaryAction={'1'}>
-                        <ListItemAvatar><SmartToyIcon /></ListItemAvatar>
-                        <ListItemText primary="ChatGPT Summary" />
-                    </ListItem>
-                    <ListItem secondaryAction={'6'}>
-                        <ListItemAvatar><PsychologyIcon /></ListItemAvatar>
-                        <ListItemText primary="AI 设置" />
-                    </ListItem>
-                </List>
+            <div className={'h-[calc(100vh-8rem)] overflow-y-auto'}>
+                <NavList activeItem={''} />
             </div>
-            <div className={'h-20'}>
+            <div className={'h-20 flex-end'}>
                 <h1 className={'font-serif text-3xl'}>
                     <ruby>鹤
                         <rp>(</rp>
---
+++ components/ui/NavList.tsx
@@ -0,0 +1,131 @@
+import RssFeedIcon from "@mui/icons-material/RssFeed"
+import { Collapse, Divider, List, ListItemText, ListItemButton, ListItemIcon } from "@mui/material"
+import AddBoxIcon from "@mui/icons-material/AddBox"
+import AutoStoriesIcon from "@mui/icons-material/AutoStories"
+import SmartToyIcon from "@mui/icons-material/SmartToy"
+import PsychologyIcon from "@mui/icons-material/Psychology"
+import * as React from "react"
+import InboxIcon from '@mui/icons-material/MoveToInbox'
+import ExpandLess from '@mui/icons-material/ExpandLess'
+import ExpandMore from '@mui/icons-material/ExpandMore'
+
+export function NavList(props: { className?: string, activeItem: string }) {
+    const [openPerson, setOpenPerson] = React.useState(true)
+    const [openOther, setOpenOther] = React.useState(false)
+    const [openKit, setOpenKit] = React.useState(false)
+
+    const handleClick = (active: string) => {
+        if (active === 'person') {
+            setOpenPerson(!openPerson)
+        } else if (active === 'other') {
+            setOpenOther(!openOther)
+        } else if (active === 'kit') {
+            setOpenKit(!openKit)
+        }
+    }
+    return (
+        <div className={`${props.className} && flex flex-col space-y-2`}>
+            <ListItemButton onClick={() => handleClick('person')}>
+                <ListItemIcon>
+                    <InboxIcon />
+                </ListItemIcon>
+                <ListItemText primary={'个人订阅'} />
+                {openPerson ? <ExpandLess /> : <ExpandMore />}
+            </ListItemButton>
+            <Collapse in={openPerson} timeout="auto" unmountOnExit>
+                <List component="div" disablePadding>
+                    <ListItemButton sx={{ pl: 2 }}>
+                        <ListItemIcon>
+                            <RssFeedIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="旅行者的随想" />
+                    </ListItemButton>
+                    <ListItemButton sx={{ pl: 2 }}>
+                        <ListItemIcon>
+                            <RssFeedIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="阮一峰周刊" />
+                    </ListItemButton>
+                    <ListItemButton sx={{ pl: 2 }}>
+                        <ListItemIcon>
+                            <RssFeedIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="螺莉莉数据中心" />
+                    </ListItemButton>
+                </List>
+            </Collapse>
+            <Divider />
+            <ListItemButton onClick={() => handleClick('other')}>
+                <ListItemIcon>
+                    <InboxIcon />
+                </ListItemIcon>
+                <ListItemText primary={'第三方订阅集'} />
+                {openOther ? <ExpandLess /> : <ExpandMore />}
+            </ListItemButton>
+            <Collapse in={openOther} timeout="auto" unmountOnExit>
+                <List component="div" disablePadding>
+                    <ListItemButton sx={{ pl: 2 }}>
+                        <ListItemIcon>
+                            <RssFeedIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="鹤鸣官方订阅集" />
+                    </ListItemButton>
+                    <ListItemButton sx={{ pl: 2 }}>
+                        <ListItemIcon>
+                            <RssFeedIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="RSSHUB 订阅集" />
+                    </ListItemButton>
+                    <ListItemButton sx={{ pl: 2 }}>
+                        <ListItemIcon>
+                            <RssFeedIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="Nature 期刊订阅" />
+                    </ListItemButton>
+                    <ListItemButton sx={{ pl: 2 }}>
+                        <ListItemIcon>
+                            <RssFeedIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="Google 学术" />
+                    </ListItemButton>
+                </List>
+            </Collapse>
+            <Divider />
+            <ListItemButton onClick={() => handleClick('kit')}>
+                <ListItemIcon>
+                    <InboxIcon />
+                </ListItemIcon>
+                <ListItemText primary={'工具栏'} />
+                {openKit ? <ExpandLess /> : <ExpandMore />}
+            </ListItemButton>
+            <Collapse in={openKit} timeout="auto" unmountOnExit>
+                <List component="div" disablePadding>
+                    <ListItemButton sx={{ pl: 2 }}>
+                        <ListItemIcon>
+                            <AddBoxIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="添加订阅" />
+                    </ListItemButton>
+                    <ListItemButton sx={{ pl: 2 }}>
+                        <ListItemIcon>
+                            <AutoStoriesIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="最近阅读" />
+                    </ListItemButton>
+                    <ListItemButton sx={{ pl: 2 }}>
+                        <ListItemIcon>
+                            <SmartToyIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="ChatGPT Summary" />
+                    </ListItemButton>
+                    <ListItemButton sx={{ pl: 2 }}>
+                        <ListItemIcon>
+                            <PsychologyIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="AI 设置" />
+                    </ListItemButton>
+                </List>
+            </Collapse>
+        </div>
+    )
+}
